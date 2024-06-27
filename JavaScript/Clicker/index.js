f0e307cfcdfedc1e7ae84f8fa50254d503a80857
Oleg Ryskin <olegrskn@gmail.com>
2024-06-27 23:08:13 +0300

    Initial commit, 1st stable version of "clicker"

---
+++ JavaScript/Clicker/index.js
@@ -0,0 +1,60 @@
+let score = 0;
+let timer = 4;
+let timerActive = false;
+
+function finishTheGame(event) {
+  const element = document.querySelector(".score");
+  element.classList.toggle("active");
+  if (event.target.classList == "score") {
+    event.target.classList.toggle("active");
+    if (!event.target.classList.contains("active")) {
+      event.target.classList.toggle("active");
+    }
+  }
+}
+
+function startTimer(timer) {
+  if (timer === 0) {
+    document.querySelector(".timer").textContent = "Time: " + timer;
+    timerActive = false;
+    finishTheGame();
+  } else {
+    document.querySelector(".timer").textContent = "Time: " + timer;
+    setTimeout(function () {
+      startTimer(timer - 1);
+    }, 1000);
+  }
+}
+function clickHandler() {
+  let newScore = ++score;
+  if (!timerActive) {
+    timerActive = true;
+    startTimer(timer);
+  }
+
+  document.querySelector(".score").textContent = "Score: " + newScore;
+}
+
+function setTime() {
+  const time = new Date();
+  const newDiv = document.createElement("div");
+  const footer = document.querySelector("footer");
+  newDiv.textContent = time.toLocaleTimeString();
+  footer.appendChild(newDiv);
+}
+function changeTime() {
+  const time = new Date();
+  const newDiv = document.querySelector("footer div");
+  newDiv.textContent = time.toLocaleTimeString();
+  setTimeout(changeTime, 1000);
+}
+function init() {
+  const button = document.querySelector(".button");
+  button.addEventListener("click", clickHandler);
+  setTime();
+  changeTime();
+  //   const score = document.querySelector(".info");
+  //   score.addEventListener("click", finishTheGame);
+}
+
+window.addEventListener("DOMContentLoaded", init);
